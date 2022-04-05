04007312552bc43573b97fce2fa82f4994b7b66b
mcmarkj <mark.mcwhirter@glean.co>
2022-04-05 10:46:23 +0100

    Fix linebreak issue

--- send-slack.py
+++ send-slack.py
@@ -5,6 +5,7 @@ import sys
 
 channels_list = {}
 
+
 def fetch_channels(client: WebClient, next_cursor: str = ""):
     try:
         result = client.conversations_list(
@@ -21,7 +22,12 @@ def fetch_channels(client: WebClient, next_cursor: str = ""):
 
 
 def validate_vars():
-    MANDATORY_ENV_VARS = ["SLACK_CHANNEL", "MESSAGE_CONTENT", "PIPELINE_NAME", "SLACK_BOT_TOKEN"]
+    MANDATORY_ENV_VARS = [
+        "SLACK_CHANNEL",
+        "MESSAGE_CONTENT",
+        "PIPELINE_NAME",
+        "SLACK_BOT_TOKEN",
+    ]
     for var in MANDATORY_ENV_VARS:
         if var not in os.environ:
             raise EnvironmentError("Error. Env var {} is not set and is required to run.".format(var))
@@ -31,9 +37,13 @@ def main():
     validate_vars()
 
     slack_channel = os.environ["SLACK_CHANNEL"]
-    message_content = os.environ["MESSAGE_CONTENT"]
+    message_content = os.environ["MESSAGE_CONTENT"].replace(r"\n", "\n")
     pipeline_name = os.environ["PIPELINE_NAME"]
     slack_bot_token = os.environ["SLACK_BOT_TOKEN"]
+    slack_icon = os.getenv(
+        "SLACK_BOT_ICON",
+        "https://avatars.slack-edge.com/2021-11-25/2761902347286_e28cd3f1518d297d034b_512.png",
+    )
 
     try:
         client = WebClient(token=slack_bot_token)
@@ -41,15 +51,16 @@ def main():
         # Get the channel ID from the name
         next_cursor = fetch_channels(client)["response_metadata"]["next_cursor"]
         while next_cursor:
-            next_cursor = fetch_channels(client, next_cursor)["response_metadata"][
-                "next_cursor"
-            ]
+            next_cursor = fetch_channels(client, next_cursor)["response_metadata"]["next_cursor"]
 
         channel_id = channels_list[slack_channel]
 
         # Send the slack message, if it fails an exception will fire
         client.chat_postMessage(
-            channel=channel_id, text=message_content, username=pipeline_name
+            channel=channel_id,
+            text=message_content,
+            username=pipeline_name,
+            icon_url=slack_icon,
         )
         print("Message sent.")
     except SlackApiError as e:
@@ -60,6 +71,7 @@ def main():
             f"Channel {slack_channel} not found. If it a private channel: Mention @Github_Actions & invite them to the channel first"
         )
         sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
